38fdc3eaf98be8b2c33b3c32e4db94300e63fe36
Andrey Sorokin <sorokin0andrey@gmail.com>
2022-11-03 12:08:43 +0600

    add ILetoAppInjected interface

---
+++ app.ts
@@ -0,0 +1,57 @@
+export enum HabticFeebbackTypes {
+  selection = "selection",
+  impactLight = "impactLight",
+  notificationSuccess = "notificationSuccess",
+  notificationError = "notificationError",
+}
+
+export enum LetoAppPlatform {
+  ios = "ios",
+  android = "android",
+  macos = "macos",
+}
+
+export enum LetoAppTheme {
+  dark = "dark",
+  light = "light",
+}
+
+export interface ISafeAreaInsets {
+  top: number;
+  right: number;
+  bottom: number;
+  left: number;
+}
+
+export type LetoAppStateStatus =
+  | "active"
+  | "background"
+  | "inactive"
+  | "unknown"
+  | "extension";
+
+export interface ILetoAppChangeThemeEvent {
+  event: "change_theme";
+  payload: {
+    theme: LetoAppTheme;
+  };
+}
+
+export interface ILetoAppStateEvent {
+  event: "app_state";
+  payload: {
+    appState: LetoAppStateStatus;
+  };
+}
+
+export type ILetoAppEvent = ILetoAppChangeThemeEvent | ILetoAppStateEvent;
+
+/** window.letoApp */
+export interface ILetoAppInjected {
+  version: string;
+  initialTheme: LetoAppTheme;
+  platform: LetoAppPlatform;
+  safeAreaInsets: ISafeAreaInsets;
+  hapticFeedback: (type: HabticFeebbackTypes) => Promise<void>;
+  listen: (callback: (event: ILetoAppEvent) => void) => () => void;
+}
